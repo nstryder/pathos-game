1010ef1d4505fb1f4ca1c097bb8037f807af404a
portavarise <parasyte217@gmail.com>
2026-03-27 12:55:14 -0700

    Fix Bite not resetting stats

--- src/resources/effect_behaviors/S000-E01-N004_bite.gd
+++ src/resources/effect_behaviors/S000-E01-N004_bite.gd
@@ -6,3 +6,7 @@ extends EffectBehavior
 func enter(data: GameData) -> void:
 	data.target_entity.add_condition("uid://5wlge7x6o2s")
 	data.target_entity.current_attack += 1
+
+
+func exit(data: GameData) -> void:
+	data.target_entity.current_attack -= 1
