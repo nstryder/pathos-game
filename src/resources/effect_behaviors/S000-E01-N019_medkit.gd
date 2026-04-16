005923563ce6228abb9636ebc21216f9d1a0dbf4
portavarise <parasyte217@gmail.com>
2026-04-16 16:46:12 -0700

    Remove leftover legacy behaviors

--- src/resources/effect_behaviors/S000-E01-N019_medkit.gd
+++
@@ -1,6 +0,0 @@
-extends EffectBehavior
-
-const HEAL_AMOUNT = 3
-
-func enter(data: GameData) -> void:
-	data.target_entity.heal(HEAL_AMOUNT)
--- src/resources/effect_behaviors/S000-E01-N037_treasure.gd
+++
@@ -1,6 +0,0 @@
-extends EffectBehavior
-
-
-func enter(data: GameData) -> void:
-    data.effect_player.draw_effects(1)
-    data.server.sync_client_hands()
