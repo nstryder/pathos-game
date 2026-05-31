6944a885fc5d2d47f521973b7f47872918197751
portavarise <parasyte217@gmail.com>
2026-05-31 15:26:16 -0700

    EN: Haru

--- src/resources/entity_abilities/S001-E01-N006_haru.gd
+++ src/resources/entity_abilities/S001-E01-N006_haru.gd
@@ -1,1 +1,7 @@
 extends EntityAbility
+
+
+# Shuffle your Effect discard pile back into your Effect draw pile
+func activate(game_data: GameData) -> void:
+    user.player.return_discards_to_deck()
+    game_data.server.sync_client_hands()
