9218ed2beb1d410e54fbc2adc21bb0fa8cec97d4
nstryder <parasyte217@gmail.com>
2026-06-15 17:58:04 -0700

    Add pre-damage condition hook

--- src/resources/entity_conditions/_base_condition.gd
+++ src/resources/entity_conditions/_base_condition.gd
@@ -5,5 +5,10 @@ class_name Condition
 
 
 @warning_ignore("unused_parameter")
+func on_pre_damage_given(attacker: EntityCard, defender: EntityCard) -> void:
+	pass
+
+
+@warning_ignore("unused_parameter")
 func on_post_damage_given(attacker: EntityCard, defender: EntityCard) -> void:
 	pass
