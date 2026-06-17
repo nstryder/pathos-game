85c80a44e07f0d1d0bc797e7fcabf1a7413ee591
nstryder <parasyte217@gmail.com>
2026-06-17 16:08:54 -0700

    EN: Akari

--- src/resources/entity_abilities/S001-E01-N007_akari.gd
+++ src/resources/entity_abilities/S001-E01-N007_akari.gd
@@ -1,1 +1,7 @@
 extends EntityAbility
+
+# Negate the enemy Entity's ability in combat.
+func activate(game_data: GameData) -> void:
+    var enemy: EntityCard = game_data.server.combat_manager.get_opposing_entity_to(user)
+    if enemy:
+        game_data.combat_data.entities[enemy].can_use_ability = false
