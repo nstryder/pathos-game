3d84a4b39e5d0f9ae9e6d29570e17703a6f14027
nstryder <parasyte217@gmail.com>
2026-06-17 15:59:27 -0700

    Remove unused hijack_damage variable from Condition

--- src/resources/entity_conditions/_base_condition.gd
+++ src/resources/entity_conditions/_base_condition.gd
@@ -1,8 +1,6 @@
 extends Node2D
 class_name Condition
 
-@export var hijacks_damage_calculation: bool = false
-
 
 @warning_ignore("unused_parameter")
 func on_pre_damage_given(attacker: EntityCard, defender: EntityCard) -> void:
