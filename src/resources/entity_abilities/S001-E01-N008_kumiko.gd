72df987ad58397c4da921f02d3ddff04bc890782
nstryder <parasyte217@gmail.com>
2026-07-06 16:33:10 -0700

    EN: Kumiko

--- src/resources/entity_abilities/S001-E01-N008_kumiko.gd
+++ src/resources/entity_abilities/S001-E01-N008_kumiko.gd
@@ -1,1 +1,6 @@
 extends EntityAbility
+
+
+# Keep attached Effect cards until the next turn.
+func activate(game_data: GameData) -> void:
+    game_data.combat_data.entities[user].keep_fx_attach_longer = true
