6a91cc46a567e0d66dc46ee9249d660e38778713
nstryder <parasyte217@gmail.com>
2026-07-08 14:04:03 -0700

    EN: Shin

--- src/resources/entity_abilities/S001-E01-N002_shin.gd
+++ src/resources/entity_abilities/S001-E01-N002_shin.gd
@@ -1,1 +1,5 @@
 extends EntityAbility
+
+# Deal 3 DMG back at the enemy Entity after being hit in combat.
+func activate(_game_data: GameData) -> void:
+    user.add_condition("res://src/resources/entity_conditions/counterattack.tscn")
---
+++ src/resources/entity_conditions/counterattack.gd
@@ -0,0 +1,5 @@
+extends Condition
+
+# Deal 3 DMG back at the enemy Entity after being hit in combat.
+func on_post_damage_taken(attacker: EntityCard, _defender: EntityCard) -> void:
+	combat_manager.deal_global_damage(attacker, 3)
