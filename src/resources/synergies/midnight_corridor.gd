8b52e64a87b5d9e47652074b68ee499247f8ef1a
nstryder <parasyte217@gmail.com>
2026-08-11 15:38:53 -0700

    SYN: Midnight Corridor

--- src/resources/synergies/midnight_corridor.gd
+++ src/resources/synergies/midnight_corridor.gd
@@ -1,5 +1,7 @@
 extends SynergyBehavior
 
 
+# Draw 1 Effect card every time Synergy activates.
 func on_activate() -> void:
-    pass
+    print("Midnight Corridor activated!")
+    player.draw_effects(1)
